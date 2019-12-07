149812f58ba6ab47630f31bb6b11132840f68520
tomjmcnamee <tom@tomjmcnamee.com>
2019-12-07 16:27:50 -0500

    correct env variable for backend references

---
+++ build/precache-manifest.7f0000ffa5ee7e07cd3c9ad337cdd8e5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f201612d94213243944e46c1ca41da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c4f829c30d6bc49fda5",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "82531bf66fe5be11dc2d",
+    "url": "/static/css/main.2c8d1c18.chunk.css"
+  },
+  {
+    "revision": "6c4f829c30d6bc49fda5",
+    "url": "/static/js/2.e96e39dc.chunk.js"
+  },
+  {
+    "revision": "82531bf66fe5be11dc2d",
+    "url": "/static/js/main.5b5e6868.chunk.js"
+  },
+  {
+    "revision": "c8653428dfdd708e558b",
+    "url": "/static/js/runtime-main.a5a11cd9.js"
+  },
+  {
+    "revision": "d4141fc742896c2b2b33ea9617455d87",
+    "url": "/static/media/D2.d4141fc7.png"
+  },
+  {
+    "revision": "cbd6d31fee0a70e3be130bd196951a63",
+    "url": "/static/media/D3.cbd6d31f.png"
+  },
+  {
+    "revision": "0b3f233991daf8d155ffb8afe80e3d3f",
+    "url": "/static/media/D4.0b3f2339.png"
+  },
+  {
+    "revision": "9e333679c05d80c4c1d3a403523a29d7",
+    "url": "/static/media/D5.9e333679.png"
+  },
+  {
+    "revision": "4c9b990c4c51066bf41a103fa75d2d86",
+    "url": "/static/media/D6.4c9b990c.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
--- src/Containers/homepage.js
+++ src/Containers/homepage.js
@@ -1,148 +1,148 @@
-import React from 'react';
-import GameBoard from '../Components/stb-gameBoard'
-import NumberTiles from '../Components/stb-numberTiles'
-import DiceRoll from '../Components/stb-diceRoll'
-import CreateNumberTile from '../Components/stb-createNumberTile'
-import { connect } from 'react-redux'
-import { logUserIn, logOut, autoLogIn } from '../actions'
+// import React from 'react';
+// import GameBoard from '../Components/stb-gameBoard'
+// import NumberTiles from '../Components/stb-numberTiles'
+// import DiceRoll from '../Components/stb-diceRoll'
+// import CreateNumberTile from '../Components/stb-createNumberTile'
+// import { connect } from 'react-redux'
+// import { logUserIn, logOut, autoLogIn } from '../actions'
 
 
 
 
-class Homepage extends React.Component {
-  state = {
-    board: [1,2,3,4,5,6,7,8,9,10,11,12],
-    comboArray: [],
-    rollSum: 0,
-    gameOver: false
-  }
-  componentDidMount(){
-    document.title = "TJM - Shut The Box" 
-    this.props.autoLogIn()   
-  }  // ends component did
+// class Homepage extends React.Component {
+//   state = {
+//     board: [1,2,3,4,5,6,7,8,9,10,11,12],
+//     comboArray: [],
+//     rollSum: 0,
+//     gameOver: false
+//   }
+//   componentDidMount(){
+//     document.title = "TJM - Shut The Box" 
+//     this.props.autoLogIn()   
+//   }  // ends component did
 
 
 
 
-  youLose = () => {
-    //function for when the player LOSES
-    console.log("You Lose")
-    this.setState({
-      gameOver: true
-    })
-  }
+//   youLose = () => {
+//     //function for when the player LOSES
+//     console.log("You Lose")
+//     this.setState({
+//       gameOver: true
+//     })
+//   }
 
-  youWin = () => {
-    //function for when the player WINS
-  }
+//   youWin = () => {
+//     //function for when the player WINS
+//   }
 
-  twoSum = function (arr, target) {
-    let result = [];
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = i + 1; j < arr.length; j++) {
-        if (arr[i] + arr[j] === target) {
-            result.push(arr[i]);
-            result.push(arr[j]);
-        }
-      }
-    }
-    return result;
-  } // ends twoSum function 
+//   twoSum = function (arr, target) {
+//     let result = [];
+//     for (let i = 0; i < arr.length; i++) {
+//       for (let j = i + 1; j < arr.length; j++) {
+//         if (arr[i] + arr[j] === target) {
+//             result.push(arr[i]);
+//             result.push(arr[j]);
+//         }
+//       }
+//     }
+//     return result;
+//   } // ends twoSum function 
 
-  rollHandler = (rollSum) => {
-    console.log("roll handler -rollsum: ", rollSum)
-    let newComboArray = this.twoSum(this.state.board, rollSum)
-    if (this.state.board.indexOf(rollSum) > -1) {
-      newComboArray.push(rollSum)
-    }
-    if (newComboArray.length == 0) {
-      this.youLose()
-    } else {
-      this.setState({
-        comboArray: newComboArray,
-        rollSum: rollSum
-      })
-    }
-  } // closes rollHandler
+//   rollHandler = (rollSum) => {
+//     console.log("roll handler -rollsum: ", rollSum)
+//     let newComboArray = this.twoSum(this.state.board, rollSum)
+//     if (this.state.board.indexOf(rollSum) > -1) {
+//       newComboArray.push(rollSum)
+//     }
+//     if (newComboArray.length == 0) {
+//       this.youLose()
+//     } else {
+//       this.setState({
+//         comboArray: newComboArray,
+//         rollSum: rollSum
+//       })
+//     }
+//   } // closes rollHandler
 
-  numberClickHandler = (number) => {
-    // event.preventDefault()
-    console.log("click handler", number)
-    if (this.state.comboArray[this.state.comboArray.length -1] == number &&  this.state.comboArray.length%2 == 1) {
-      let newBoard = this.state.board
-      let indexToDel = newBoard.indexOf(number);
-      newBoard.splice(indexToDel, 1);
-      this.setState({
-        board: newBoard,
-        comboArray: []
-      })
-    } else {
-      let newBoard = this.state.board
-      let indexToDel = newBoard.indexOf(number);
-      newBoard.splice(indexToDel, 1);
-      let newComboArray = [this.state.rollSum - number]
-      this.setState({
-        board: newBoard,
-        comboArray: newComboArray,
-      })
+//   numberClickHandler = (number) => {
+//     // event.preventDefault()
+//     console.log("click handler", number)
+//     if (this.state.comboArray[this.state.comboArray.length -1] == number &&  this.state.comboArray.length%2 == 1) {
+//       let newBoard = this.state.board
+//       let indexToDel = newBoard.indexOf(number);
+//       newBoard.splice(indexToDel, 1);
+//       this.setState({
+//         board: newBoard,
+//         comboArray: []
+//       })
+//     } else {
+//       let newBoard = this.state.board
+//       let indexToDel = newBoard.indexOf(number);
+//       newBoard.splice(indexToDel, 1);
+//       let newComboArray = [this.state.rollSum - number]
+//       this.setState({
+//         board: newBoard,
+//         comboArray: newComboArray,
+//       })
 
-    }
-  }
+//     }
+//   }
   
-  newGameHandler = () => {
-    this.setState({
-      board: [1,2,3,4,5,6,7,8,9,10,11,12],
-      comboArray: [],
-      rollSum: 0,
-      gameOver: false
-    })
-  }
+//   newGameHandler = () => {
+//     this.setState({
+//       board: [1,2,3,4,5,6,7,8,9,10,11,12],
+//       comboArray: [],
+//       rollSum: 0,
+//       gameOver: false
+//     })
+//   }
   
   
-  numberTiles = () => { 
-    // debugger
-    return [1,2,3,4,5,6,7,8,9,10,11,12].map(num => <CreateNumberTile 
-      key={num} 
-      number={num} 
-      history={this.props.history} 
-      NumberAvailable={this.state.board.includes(num)}
-      NumberValidOption={this.state.comboArray.includes(num)}
-      FirstRun={this.state.rollSum == 0}
-      numberClickHandler={this.numberClickHandler} />)
-  }
+//   numberTiles = () => { 
+//     // debugger
+//     return [1,2,3,4,5,6,7,8,9,10,11,12].map(num => <CreateNumberTile 
+//       key={num} 
+//       number={num} 
+//       history={this.props.history} 
+//       NumberAvailable={this.state.board.includes(num)}
+//       NumberValidOption={this.state.comboArray.includes(num)}
+//       FirstRun={this.state.rollSum == 0}
+//       numberClickHandler={this.numberClickHandler} />)
+//   }
 
-render() {
-  console.log("comboarr = ", this.state.comboArray)
-
-
+// render() {
+//   console.log("comboarr = ", this.state.comboArray)
 
 
 
-        //This is where I have to see if any TWO tiles add up to rollSum
-      // } else {
-      //   this.props.youLose()
-      // }
+
+
+//         //This is where I have to see if any TWO tiles add up to rollSum
+//       // } else {
+//       //   this.props.youLose()
+//       // }
   
-    return(
+//     return(
 
-      <div id="master-div">
-        <DiceRoll rollHandler={this.rollHandler} newGameHandler={this.newGameHandler} gameOver={this.state.gameOver} board={this.state.board} />
-       {this.numberTiles()}
-      </div> // closes parent div
-    ) // closes RETURN
-  } //closes RENDER
+//       <div id="master-div">
+//         <DiceRoll rollHandler={this.rollHandler} newGameHandler={this.newGameHandler} gameOver={this.state.gameOver} board={this.state.board} />
+//        {this.numberTiles()}
+//       </div> // closes parent div
+//     ) // closes RETURN
+//   } //closes RENDER
 
 
-}  // closes APP
-function mdp(dispatch) { 
-  return { 
+// }  // closes APP
+// function mdp(dispatch) { 
+//   return { 
 
-  }
-}
+//   }
+// }
 
-{/* // this comes from reduct.js - K is local reference, V is foreign state attribute */}
-function msp(state) { return { 
+// {/* // this comes from reduct.js - K is local reference, V is foreign state attribute */}
+// function msp(state) { return { 
 
-}}
+// }}
 
-export default connect(msp, mdp)(Homepage)
+// export default connect(msp, mdp)(Homepage)
